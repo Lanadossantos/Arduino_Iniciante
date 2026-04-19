0056e3cdb33601b6d824da94f6d98106618e6290
Alana Santos <112086580+Lanadossantos@users.noreply.github.com>
2026-04-19 15:02:39 -0300

    Change potentiometer variable type to float

--- Aula3.ino
+++ Aula3.ino
@@ -5,15 +5,16 @@ AnalogRead ler de 0 a 1023 (0-1023) 2 bytes
 AnalogWrite escreve de 0 a 255 (PWM) 1 byte
 */
 
-int potenciometro = A0;
+float potenciometro = A0;
 int LED = 9;
 
 void setup(){
     pinMode(LED, OUTPUT);
+    pinMode(potenciometro, INPUT);
 }
 
 void loop(){
-    int valorPotenciometro = analogRead(potenciometro);
-    int brilho = valorPotenciometro / 4; //1023 ÷ 4 ≈ 255
+    float valorPotenciometro = analogRead(potenciometro);
+    float brilho = valorPotenciometro / 4; //1023 ÷ 4 ≈ 255
     analogWrite(LED, brilho);
 }
